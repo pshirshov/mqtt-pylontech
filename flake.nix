2986128203af4b0e184049eaf8c806276d28e3fe
Pavel Shirshov <pshirshov@eml.cc>
2026-09-18 17:26:56 +0100

    Move nixpkgs 3e3f3c7f → e554fab7

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
